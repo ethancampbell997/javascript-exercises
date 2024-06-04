37243a42d78e92a04f0747fd32add3de2118cdbe
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-03 22:39:33 -0400

    Finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    fin = "";
+    for(let i = 0; i < num; i++){
+        fin+=str;
+    }
+    return fin;
 };
 
 // Do not edit below this line
