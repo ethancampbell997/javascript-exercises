aa1f084d91f7833f70bce3dbcdec5b6403ae56ce
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-03 23:22:45 -0400

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(small, large) {
+    if(small < 0 || large < 0 || typeof small != "number" || typeof large != "number"){
+        return "ERROR";
+    }
+    let num = 0;
+    if(small > large){
+        let temp = small;
+        small=large;
+        large=temp;
+    }
+    for(let i = small; i <= large; i++){
+        num+=i;
+    }
+    return num;
 };
 
 // Do not edit below this line
