260f6ebfc576578d273bbf442f9d7b169e694972
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-03 23:14:54 -0400

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        let equ = 0;
+        for(const current of num){
+            if(arr[i] === current){
+                equ++;
+            }
+        }
+        if(equ===0){
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
 };
 
 // Do not edit below this line
