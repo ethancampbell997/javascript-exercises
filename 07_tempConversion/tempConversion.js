93a1edf0c3287578c852cc664316e97e41e499d9
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-03 23:38:10 -0400

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let cels = fahren - 32;
+  cels *= 5;
+  cels /= 9;
+  cels = Math.round(cels * 10) / 10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahren = cels * 9;
+  fahren /= 5;
+  fahren += 32;
+  fahren = Math.round(fahren * 10) / 10;
+  return fahren;
 };
 
 // Do not edit below this line
